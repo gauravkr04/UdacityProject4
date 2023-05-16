448071cfe7bfb9f918cc72b9bef1b943dd5dbe57
EC2 Default User <ec2-user@ip-172-31-81-18.ec2.internal>
2023-05-16 02:06:38 +0000

    New files added for Docker

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=gauravkr04/predict
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login
+docker tag  83738c9e8640 $dockerpath:v1.0.0
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath:v1.0.0
